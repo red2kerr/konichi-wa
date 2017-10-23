304add8e5ba85789e5bd647641dac006e343d8af
Charles E. Kerr <captainmilkweed@gmail.com>
2017-10-23 09:18:42 -0700

    Add files via upload
    23 OCT2017: Added 2 files.  Files are short R scripts written to facilitate installation of different modules for the SWIRL package.

---
+++ Swirl course installer v2.R
@@ -0,0 +1,3 @@
+library(swirl)
+src_path="C:/Users/cekerr.HSL/Downloads/swirl_courses-master.zip"
+install_course_zip(src_path, multi = TRUE)
---
+++ Swirl course installer.R
@@ -0,0 +1,9 @@
+library(swirl)
+install_course("R_Programming")
+install_course("Data_Analysis")
+install_course("Expolratory_Data_Analysis")
+install_course("Getting_and_Cleaning_Data")
+install_course("Overview_of_Statistics")
+install_course("Regression_Models")
+install_course("Statistical_Inference")
+
